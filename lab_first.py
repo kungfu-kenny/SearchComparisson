da1edcc1a0b14f9f938b4e626b1f7c9406391442
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2021-10-19 18:33:53 +0300

    produced basic values of the decision trees; started writing of the basic modelling

--- lab_first.py
+++ lab_first.py
@@ -1,5 +1,5 @@
 import re
-import requests
+import random
 from pprint import pprint
 from parse_files import ProduceSearches
 
@@ -9,7 +9,10 @@ class LabFirst:
     class which is dedicated to return all results from the labs
     """
     def __init__(self, value_list:list) -> None:
-        self.search = ProduceSearches(value_list)
+        self.inserted_list = value_list
+        self.x_1, self.x_2 = 0.34, 0.47
+        self.objective_mark, self.self_mark = 0.5, 0.75
+        self.search = ProduceSearches(self.inserted_list)
         self.search_list = self.produce_additional_sorting(
                                 self.search.produce_main())
         self.search_links = self.produce_lists_every_values()
@@ -58,28 +61,10 @@ class LabFirst:
         Output: we created values of the modified board values which were further parsed
         """
         value_lists = list(zip(*self.search_list))
-        return 
-
-    def make_aggregation_condorse(self) -> list:
-        """
-        Method which is dedicated to produce condorce aggregation of the values
-        Input:  None
-        Output: we created values of the condorse values which were further parsed
-        """
-        value_lists = list(zip(*self.search_list))
         value_len = [[len(f.get('links', [])) for f in k] for k in value_lists]
         value_len_max = [max(i) for i in value_len]
-        #TODO get values of the for to for the values
-        #TODO get values for it
-        pprint(value_lists[0])
-        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-        pprint(value_len)
-        print('00000000000000000000000000000000000000000000000000000000')
-        print(value_len_max)
-        # pprint(self.search_links[0])
-        print('22222222222222222222222222222222222222222222222222222222222')
         value_result = []
-        for value_list, link_list in zip(value_lists[:1], self.search_links[:1]):
+        for value_list, link_list in zip(value_lists[:], self.search_links[:]):
             value_list_presence = []
             for value_links_dict in value_list:
                 value_links = value_links_dict.get('links', [])
@@ -91,7 +76,6 @@ class LabFirst:
                         value_link_presence.append(-1)
                 value_list_presence.append(value_link_presence) 
             value_result.append(value_list_presence)
-        print(value_result)
         value_presence = []
         for value_searched_list, value_max in zip(value_result, value_len_max):
             value_presence_artist = []
@@ -104,20 +88,131 @@ class LabFirst:
                         value_presence_link.append(0)
                 value_presence_artist.append(value_presence_link)
             value_presence.append(value_presence_artist)
-        print('######################################################################')
-        print(value_presence[0])
-        print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-        return
+        value_results = []
+        for value_links, values_sums in zip(self.search_links, value_presence):
+            value_dict_sums = {i:0 for i in value_links}
+            for values_sum in values_sums:
+                for i, s in zip(value_links, values_sum):
+                    value_dict_sums[i] = value_dict_sums[i] + s
+            value_results.append([f for f, _ in sorted(value_dict_sums.items(), 
+                            reverse=True, key=lambda item: item[1])])
+        return value_results
+        
+    def make_aggregation_condorse(self) -> list:
+        """
+        Method which is dedicated to produce condorce aggregation of the values
+        Input:  None
+        Output: we created values of the condorse values which were further parsed
+        """
+        value_lists = list(zip(*self.search_list))
+        value_len = [[len(f.get('links', [])) for f in k] for k in value_lists]
+        value_len_max = [max(i) for i in value_len]
+        value_result = []
+        for value_list, link_list in zip(value_lists[:], self.search_links[:]):
+            value_list_presence = []
+            for value_links_dict in value_list:
+                value_links = value_links_dict.get('links', [])
+                value_link_presence = []
+                for value_link in link_list:
+                    if value_link in value_links:
+                        value_link_presence.append(value_links.index(value_link))
+                    else:
+                        value_link_presence.append(-1)
+                value_list_presence.append(value_link_presence) 
+            value_result.append(value_list_presence)
+        value_presence = []
+        for value_searched_list, value_max in zip(value_result, value_len_max):
+            value_presence_artist = []
+            for value_artist_list in value_searched_list:
+                value_presence_link = []
+                for value_index in value_artist_list:
+                    if value_index >= 0:
+                        value_presence_link.append(value_max-value_index)
+                    else:
+                        value_presence_link.append(0)
+                value_presence_artist.append(value_presence_link)
+            value_presence.append(value_presence_artist)
+        value_order = []
+        for value_searched_types, value_searched in zip(value_presence, self.search_links):
+            value_max, value_max_link = 0, 0
+            value_list = []
+            value_searched_type = value_searched.copy()
+            while len(value_searched_type) > 1:
+                for i, _ in enumerate(value_searched_type):
+                    for j, _ in enumerate(value_searched_type):
+                        if i != j:
+                            value_a = sum([k[i] for k in value_searched_types])
+                            value_b = sum([k[j] for k in value_searched_types])
+                            value_max_el, value_max_v = (i, value_a) if value_a > value_b else (j, value_b)
+                            if value_max_v > value_max:
+                                value_max = value_max_v
+                                value_max_link = value_max_el
+                max_link = value_searched_type.pop(value_max_link)  
+                value_max = 0
+                value_list.append(max_link)
+            value_list.append(value_searched_type[0])
+            value_order.append(value_list)
+        return value_order
+
+    def calculate_relevant_weight_quantity_quality(self, value_list:list) -> list:
+        """
+        Method which is dedicated to produce relevant values
+        Input:  value_list = list values which were developed of the aggregation
+        Output: we developed answer of relevant weights
+        """
+        pass
+
+    def calculate_relevant_weight_statistic(self, value_list:list) -> list:
+        """
+        Method which is dedicated to produce relevant values
+        Input:  value_list = list values which were developed of the aggregation
+        Output: we developed answer of relevant weights
+        """
+        pass
+
+    def produce_relevancy(self) -> list:
+        """
+        Method which is dedicated to produce the exspert relevancy of the selected values
+        Input:  all calculated values from the search
+        Outpyt: we developed values of the 
+        """
+        value_list = list(zip(*self.search_list))
+        list_relevancy = []
+        for value_searches, value_inserted in zip(value_list, self.inserted_list):
+            list_record = []
+            for value_search in value_searches:
+                value_created_rel = []
+                for value_link in value_search.get('names', []):
+                    if value_inserted.lower() in value_link.lower():
+                        value_created_rel.append(1)
+                    else:
+                        value_created_rel.append(random.random())
+                list_record.append(value_created_rel)
+            list_relevancy.append(list_record)
+        return list_relevancy
+    
+    def produce_kompetency_expert(self) -> float:
+        """
+        Method which is dedicated to develop kompetency of the experts of it
+        Input:  all calculated previously values of it
+        Output: we developed the formula of the kompetency of the experts
+        """
+        value_list = list(zip(*self.search_list))
+        value_relevancy = self.produce_relevancy()
+        pprint(value_relevancy[0])
+        print('0000000000000000000000000000000000')
+        pprint(value_list[0])
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
     def produce_main(self) -> None:
         """
         Method which is dedicated to produce main values of the results
         Input:  All used values
-        Output:
+        Output: selected values which was previously neccessary
         """
-        # self.make_aggregation_board()
-        self.make_aggregation_condorse()
-
+        # value_order_board = self.make_aggregation_board()
+        # value_order_condorse = self.make_aggregation_condorse()
+        self.produce_kompetency_expert()
 
 if __name__ == '__main__':
     a = LabFirst(['a$ap rocky', 'rihanna', 'tyler the creator', 'kendrick lamar', 'killer mike'])
